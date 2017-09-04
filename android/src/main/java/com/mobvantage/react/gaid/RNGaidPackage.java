49950adb1bf6653947a17219f542aabd88977e73
Cyril Neruchev <menandr@list.ru>
2017-09-04 16:48:29 +0200

    Remove createJSModules @ovveride - RN 0.47 compatibility

--- android/src/main/java/com/mobvantage/react/gaid/RNGaidPackage.java
+++ android/src/main/java/com/mobvantage/react/gaid/RNGaidPackage.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNGaidPackage implements ReactPackage {
-  
+
     public RNGaidPackage(){
     }
 
@@ -23,7 +23,7 @@ public class RNGaidPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
