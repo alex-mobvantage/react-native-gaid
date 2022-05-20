ca6a46b3d1e6fb6c7298c088ac86cab44fc5e23e
rnagella <nagella.ranjith@gmail.com>
2022-05-20 11:45:35 -0500

    Remove createJSModules @ovveride - RN 0.47 compatibility

--- android/src/main/java/com/mobvantage/react/gaid/RNGaidPackage.java
+++ android/src/main/java/com/mobvantage/react/gaid/RNGaidPackage.java
@@ -23,7 +23,7 @@ public class RNGaidPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
